07a999d209a84fa061c72412a0ac7d061e9fa833
Peter <plevasseur@gmail.com>
2016-07-17 20:20:58 -0400

    Finished Part 1, Chapter 1

---
+++ 02_Fixed-Size_Data/1.7-Predicates_Know_Thy_Data/Exercise_011.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise_011) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Exercise 11.
+
+;Exercise 11. Add the following line to the definitions area of DrRacket:
+;(define in "hello")
+
+;Then create an expression that converts whatever in represents to a number. For a string, it determines how long the string is; for an image, it uses the area; for a number, it decrements the number, unless it is already 0 or negative; for #true it uses 10 and for #false 20.
+
+; require
+(require 2htdp/image)
+
+; constants
+(define in "hello")
+
+; functions
+
